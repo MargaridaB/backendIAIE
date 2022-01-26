e5561ef5713d7be3765cfb004f8c9c5a3790bdf8
Ana Margarida Ribeiro Barbosa <74150124+MargaridaB@users.noreply.github.com>
2022-01-26 09:24:15 +0000

    Add files via upload

---
+++ config/connect.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+module.exports = {
+    con: mysql.createConnection({
+        host: 'remotemysql.com',
+        user: 'ZMUsHvmgaS',
+        password: '4eyRNF1ZTx',
+        database: 'ZMUsHvmgaS'
+
+    })
+}
