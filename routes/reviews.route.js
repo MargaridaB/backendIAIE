07f18986127d0920bc9c1e6751e7d7ea65192a7a
Ana Margarida Ribeiro Barbosa <74150124+MargaridaB@users.noreply.github.com>
2022-01-26 09:29:09 +0000

    Delete reviews.route.js

--- routes/reviews.route.js
+++
@@ -1,8 +0,0 @@
-const app = require('../server.js');
-const controllerReviews = require ('../controllers/reviews.controller.js');
-
-app.get('/reviews/', controllerReviews.read);
-app.get('/reviews/idreviews', controllerReviews.readID);
-app.post('/reviews/', controllerReviews.save);
-app.put('/reviews/idreviews', controllerReviews.update);
-app.delete('/reviews/idreviews', controllerReviews.deleteID);
